e60bc56003274c1b4bc1ace05ec2bc425a976920
Beila H <beilah@nycstudents.net>
2025-02-07 09:26:19 -0500

    Create vocab.py

---
+++ vocab.py
@@ -0,0 +1,17 @@
+# Strings represent characters, names, words
+name = "Kammy"
+# Integers for WHOLE numbers
+age = 14
+# Boolean is True or False, typically used for evaluation
+graduated = False
+# Floats for decimal
+money = 4.50
+
+def add(x,y):
+    print(x + y)
+# add(54,"11")
+
+# Input seeks user response and outputs that into the variable
+# Bill value will be equal to the response of the user
+bill = input("How much was the bill?")
+print(type(bill))
