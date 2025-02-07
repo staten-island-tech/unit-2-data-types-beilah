5ffa7e5aa4bd7b5aa1be13098794a201d841acb6
Beila H <beilah@nycstudents.net>
2025-02-07 09:26:22 -0500

    Create unit2.py

---
+++ unit2.py
@@ -0,0 +1,29 @@
+# Unit 2
+
+"""x = 3
+y = float(3)
+print(x,y)
+
+def tipCalculator():
+    bill = float(input("How much was the bill?"))
+    tip = int(input("How much was the tip?"))
+    totalAmtPaid = bill * (tip / 100) + bill 
+    print(f"You spent a total of {totalAmtPaid}")
+tipCalculator
+
+values = [1,2,3,4,5,6,7,8,9,10]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[5])
+
+"test"
+["t","e","s","t"]"""
+
+x = "this is a thing"
+y = x.split()
+z = y[0]
+print(y)
+print(z)
