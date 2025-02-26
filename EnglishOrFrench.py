29c77a8523bf2bcef2edd5ac193d35296288b89d
Beila H <beilah@nycstudents.net>
2025-02-26 09:27:04 -0500

    Create EnglishOrFrench.py

---
+++ EnglishOrFrench.py
@@ -0,0 +1,7 @@
+def input(string):
+    for i in string(1, string):
+        if t < 5 and T < 5:
+            print(f"French")
+        elif s < 5 and S < 5:
+            print(f"English")
+(input("The red cat sat on the mat. Why are you so sad cat? Don't ask that."))
