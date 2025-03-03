3f319b31b34db5204d1a3089b38c4d72b156a16f
Beila H <beilah@nycstudents.net>
2025-03-03 08:59:28 -0500

    Update EnglishOrFrench.py

--- EnglishOrFrench.py
+++ EnglishOrFrench.py
@@ -1,19 +1,14 @@
-some_text = "The red cat sat on the mat. Why are you so sad cat? Don't ask that."
-
-def lang(text):
+def lang(num, text):
     french = 0
     english = 0
     # these variables only exist inside the function
-    for letter in some_text:
-        if letter in some_text:
-            if letter == "s" or letter == "S":
-            # if letter == "s" or letter == "S"
-            # if letter in ["s" "S"]
+    for letter in text:
+        if letter == "s" or letter == "S":
                 french = french + 1
-        elif letter == "t" or letter == "T":
-            english += 1
-        if french >= english:
-            print('french')
-        else:
-            print('english')
-(lang("The red cat sat on the mat. Why are you so sad cat? Don't ask that."))
+        if letter == "t" or letter == "T":
+            english += 1   
+    if french >= english:
+        print("french")
+    else:
+        print("english")
+(lang(69, "Le chat dort sur le canapé près de la fenêtre."))
